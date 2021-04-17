35edd0e2c2775a0eda59da669bf4a8cacec574b5
Jordy Hulck <Jordy.Hulck@outlook.com>
2021-04-18 01:37:32 +0200

    On push branches

--- build/Build.cs
+++ build/Build.cs
@@ -12,7 +12,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
-[GitHubActions("pack", GitHubActionsImage.UbuntuLatest, InvokedTargets = new[] { nameof(Pack) }, OnPushTagsIgnore = new[] { "v*" })]
+[GitHubActions("pack", GitHubActionsImage.UbuntuLatest, InvokedTargets = new[] { nameof(Pack) }, OnPushBranches = new[] { "'*'" })]
 [GitHubActions("publish", GitHubActionsImage.UbuntuLatest, InvokedTargets = new[] { nameof(Pack), nameof(Publish) }, OnPushTags = new[] { "v*" }, ImportSecrets = new[] { nameof(NuGetApiKey) })]
 class Build : NukeBuild
 {
