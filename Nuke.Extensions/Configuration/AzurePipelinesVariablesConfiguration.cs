a7a8aa82d45f541d640c674caf19a47ff7f3f62a
Jordy Hulck <Jordy.Hulck@outlook.com>
2021-04-19 11:27:28 +0200

    Remove simple variables Doesn't seem to be supported

--- Nuke.Extensions/Configuration/AzurePipelinesVariablesConfiguration.cs
+++ Nuke.Extensions/Configuration/AzurePipelinesVariablesConfiguration.cs
@@ -11,38 +11,28 @@ namespace Megasware128.Nuke.Extensions.Configuration
 
         public override void Write(CustomFileWriter writer)
         {
-            if (VariableGroups.Length is 0)
+            var variableGroups = VariableGroups;
+            if (variableGroups.Length > 0)
             {
-                using (writer.WriteBlock("variables:"))
-                {
-                    Variables.ForEach(x => writer.WriteLine($"- {x.Name}: {x.Value}"));
-                    writer.WriteLine();
-                }
-
-                base.Write(writer);
-            }
-            else
-            {
-                var variableGroups = VariableGroups;
                 VariableGroups = Array.Empty<string>();
-
-                using (writer.WriteBlock("variables:"))
-                {
-                    variableGroups.ForEach(x => writer.WriteLine($"- group: {x}"));
-                    Variables.ForEach(x =>
-                    {
-                        using (writer.WriteBlock($"- name: {x.Name}"))
-                        {
-                            writer.WriteLine($"value: {x.Value}");
-                        }
-                    });
-                    writer.WriteLine();
-                }
-
-                base.Write(writer);
-
-                VariableGroups = variableGroups;
             }
+
+            using (writer.WriteBlock("variables:"))
+            {
+                variableGroups.ForEach(x => writer.WriteLine($"- group: {x}"));
+                Variables.ForEach(x =>
+                {
+                    using (writer.WriteBlock($"- name: {x.Name}"))
+                    {
+                        writer.WriteLine($"value: {x.Value}");
+                    }
+                });
+                writer.WriteLine();
+            }
+
+            base.Write(writer);
+
+            VariableGroups = variableGroups;
         }
     }
 }
